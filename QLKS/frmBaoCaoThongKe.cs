6afbb85716448c334bca03a42283ce4577b070f1
TNinh39 <dangninh020@gmail.com>
2024-04-05 08:46:35 +0700

    7 commit

--- QLKS/frmBaoCaoThongKe.cs
+++ QLKS/frmBaoCaoThongKe.cs
@@ -23,7 +23,7 @@ namespace QLKS
             DataSet ds = kn.LayDuLieu(query);
             dgvThongKe.DataSource = ds.Tables[0];
         }
-        // Lấy dữ liệu thống kê
+        // Lấy dữ liệu thống kê 
 
 
         public void UpdateTotalRevenueInThongKe()
