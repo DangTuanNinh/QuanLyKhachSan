86b063301b686057b204590195bd02b25366d7c7
TNinh39 <dangninh020@gmail.com>
2024-04-05 09:12:52 +0700

    34 commit

--- QLKS/frmBaoCaoThongKe.cs
+++ QLKS/frmBaoCaoThongKe.cs
@@ -16,7 +16,7 @@ namespace QLKS
             InitializeComponent();
         }
         KetNoi kn = new KetNoi();
-        // lớp kết nối abcdeaegsfggh4d23dfhhsf
+        // lớp kết nối abcdeaegsfggh4d23dfhhsfv
         public void getData()
         {
             string query = " select * from ThongKe";
